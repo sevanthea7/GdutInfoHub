6d5010cdf3ef22e31e3fe13776dd43d2c6f1d99d
ssrin <2156667970@qq.com>
2025-11-28 18:40:08 +0800

    feature: 表单通知公告使用mock数据模拟api接口数据

---
+++ gdut-info-hub/src/api/waterElectricApi.js
@@ -0,0 +1,10 @@
+// import waterElectricServiceMock from "./waterElectricServiceMock";
+
+// // 获取水电服务通知
+// getWaterElectricService: () => {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(waterElectricServiceMock);
+//     }, 500);
+//   });
+// };
---
+++ gdut-info-hub/src/mock/mockLogisticsRepairData.js
@@ -0,0 +1,62 @@
+export default {
+  code: 200,
+  msg: "success",
+  data: {
+    items: [
+      {
+        title: "关于开展宿舍家具维修的通知",
+        url: "https://www.gdut.edu.cn/info/5012/8012.htm",
+        date: "2025/11/11",
+        content:
+          "11月15日-11月30日将对全校宿舍损坏家具进行集中维修，请有需要的同学通过后勤报修系统提交维修申请。",
+        key_words: ["家具维修", "宿舍", "报修"],
+        department: "就业指导中心",
+      },
+      {
+        title: "校园道路维修施工通知",
+        url: "https://www.gdut.edu.cn/info/5013/8013.htm",
+        date: "2025/11/08",
+        content:
+          "因校园主干道沥青铺设工程，11月12日-11月20日将对东西校区连接道路进行封闭施工，请师生绕行南北通道。",
+        key_words: ["道路维修", "施工", "绕行"],
+        department: "就业指导中心",
+      },
+      {
+        title: "食堂设备升级改造通知",
+        url: "https://www.gdut.edu.cn/info/5014/8014.htm",
+        date: "2025/11/05",
+        content:
+          "第一食堂将于11月10日-11月20日进行设备升级改造，期间暂停营业，师生可前往第二、三食堂就餐。",
+        key_words: ["食堂", "设备改造", "暂停营业"],
+        department: "就业指导中心",
+      },
+      {
+        title: "后勤报修系统使用指南",
+        url: "https://www.gdut.edu.cn/info/5015/8015.htm",
+        date: "2025/11/01",
+        content:
+          "为提高报修效率，新版后勤报修系统已上线，包含在线报修、进度查询、评价反馈等功能，详细使用指南见附件。",
+        key_words: ["报修系统", "使用指南", "后勤"],
+        department: "就业指导中心",
+      },
+      {
+        title: "校园绿化维护通知",
+        url: "https://www.gdut.edu.cn/info/5016/8016.htm",
+        date: "2025/10/28",
+        content:
+          "11月1日-11月10日将对校园绿化带进行修剪维护，作业期间部分区域将临时围挡，请师生注意安全。",
+        key_words: ["绿化维护", "修剪", "安全"],
+        department: "就业指导中心",
+      },
+      {
+        title: "关于加强校园卫生管理的通知",
+        url: "https://www.gdut.edu.cn/info/5017/8017.htm",
+        date: "2025/10/25",
+        content:
+          "为创建文明校园，将加强校园卫生检查力度，增设分类垃圾桶，严禁随手乱扔垃圾，共同维护校园环境。",
+        key_words: ["卫生管理", "文明校园", "垃圾分类"],
+        department: "就业指导中心",
+      },
+    ],
+  },
+};
