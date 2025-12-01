57cb8c820b454d63a95d363a57da3c52b51d89fb
sevanthea7 <ec3194778264@gmail.com>
2025-12-01 20:22:51 +0800

    fix: 修正接口对接函数传递内容(#6)

--- src/app/search_by_label.py
+++ src/app/search_by_label.py
@@ -1,5 +1,21 @@
 import json
 import os
+import re
+
+def normalize_date(date_str):
+    if not date_str:
+        return ""
+
+    parts = re.findall(r"\d+", date_str)
+    if len(parts) >= 3:
+        year, month, day = parts[:3]
+        # 补零（保证两位数）
+        month = month.zfill(2)
+        day = day.zfill(2)
+        return f"{year}/{month}/{day}"
+    
+    return date_str
+
 
 def search_news_by_label(label, folder_path):
     matched_contents = []
@@ -17,10 +33,21 @@ def search_news_by_label(label, folder_path):
                 if not isinstance(news_label, str):
                     continue
                 if label == news_label:
-                    matched_contents.append(news)
+                    # matched_contents.append(news)
+                    cleaned_news = dict(news)   # 复制一份，避免直接修改原数据
+                    # 删除字段
+                    cleaned_news.pop("label", None)
+                    cleaned_news.pop("keywords", None)
+                    if label != "通知公告" and "column" in cleaned_news: 
+                        cleaned_news.pop("column", None)
+                    if "date" in cleaned_news:
+                        cleaned_news["date"] = normalize_date(cleaned_news["date"])
+                    matched_contents.append(cleaned_news)
+
     num_lb = len(matched_contents)
     print(f'查找到 {label} 内容共 {num_lb} 条！')
+    print(matched_contents[0])
     return matched_contents
 
 
-# search_news_by_label('水电服务', 'src/crawler/news_data')
+search_news_by_label('通知公告', 'src/crawler/news_data')
